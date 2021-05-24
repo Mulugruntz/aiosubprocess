4b12a8a3b3996a4510f64919542ded3493ed5066
Samuel Giffard <samuel@giffard.co>
2021-05-24 22:04:59 +0900

    Added Windows specific tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,28 @@ from pathlib import Path
 import sys
 import logging
 
-if sys.platform == "win32":
-    asyncio.set_event_loop_policy(
-        asyncio.WindowsProactorEventLoopPolicy()
-    )
+import pytest
+
+if sys.platform.startswith("win"):
+    if sys.version_info < (3, 7):
+        # Python 3.6 has no WindowsProactorEventLoopPolicy class
+        from asyncio import events
+
+        class WindowsProactorEventLoopPolicy(events.BaseDefaultEventLoopPolicy):
+            _loop_factory = asyncio.ProactorEventLoop
+
+    else:
+        WindowsProactorEventLoopPolicy = asyncio.WindowsProactorEventLoopPolicy
+
+
+@pytest.fixture
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    if sys.platform.startswith("win"):
+        asyncio.set_event_loop_policy(WindowsProactorEventLoopPolicy())
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
 
 # it makes it easier if all tests are forced to
 # have the package root as the CWD.
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,8 @@
 import asyncio
+import sys
 from datetime import timedelta, datetime
 from itertools import zip_longest
+from typing import Union
 
 import pytest
 from aiosubprocess import Process
@@ -12,33 +14,43 @@ except ImportError:  # pragma: no cover
     all_tasks = asyncio.Task.all_tasks
 
 
+def get_command(cmd: Union[str, list]) -> Union[str, list]:  # pragma: no cover
+    if not sys.platform.startswith("win"):
+        return cmd
+    if isinstance(cmd, str):
+        return cmd.replace(";", "&")
+    if isinstance(cmd, list):
+        return list(map(lambda s: s.replace(";", "&"), cmd))
+    return cmd
+
+
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="does not run on windows")
 @pytest.mark.asyncio
-async def test_double_shell():
-    loop = asyncio.get_event_loop()
+async def test_double_shell(event_loop):
     reader = Process(
         """for i in {1..5}
         do
            echo "Hello $i World" > tempfile.log
            sleep 0.1
         done""",
-        loop=loop,
+        loop=event_loop,
         name="Writer",
     )
     writer = Process(
         "timeout --foreground 1s tail -f tempfile.log",
-        loop=loop,
+        loop=event_loop,
         name="Reader",
         expected_returncode=124,  # Because timeout is expected
         sleep_resolution=0.05,
     )
     awaitable_reader = reader.shell()
     awaitable_writer = writer.shell()
-    gathered = asyncio.gather(awaitable_reader, awaitable_writer, loop=loop)
+    gathered = asyncio.gather(awaitable_reader, awaitable_writer, loop=event_loop)
     await gathered
     assert gathered.result() == [True, True]
     deleter = Process(
         "unlink tempfile.log",
-        loop=loop,
+        loop=event_loop,
         name="Deleter",
     )
     result = await deleter.shell()
@@ -55,11 +67,9 @@ async def test_double_shell():
         pytest.param("exit 1", 1, True, id="retcode_nok_success"),
     ],
 )
-async def test_return_codes(cmd, expected_returncode, expected_result):
-    retcode_sub = Process(
-        cmd,
-        expected_returncode=expected_returncode
-    )
+async def test_return_codes(event_loop, cmd, expected_returncode, expected_result):
+    cmd = get_command(cmd)
+    retcode_sub = Process(cmd, loop=event_loop, expected_returncode=expected_returncode)
     result = await retcode_sub.shell()
     assert result is expected_result
 
@@ -79,15 +89,15 @@ async def test_return_codes(cmd, expected_returncode, expected_result):
         ),
     ],
 )
-async def test_shell(cmd, expected_stdout, expected_stderr):
-    loop = asyncio.get_event_loop()
+async def test_shell(event_loop, cmd, expected_stdout, expected_stderr):
+    cmd = get_command(cmd)
     output, errors = [], []
     aiosub = Process(
         *([cmd] if isinstance(cmd, str) else cmd),
-        loop=loop,
+        loop=event_loop,
         stdout=lambda s: output.append(s),
         stderr=lambda s: errors.append(s),
-        with_prefix=False
+        with_prefix=False,
     )
     success = await aiosub.shell()
     assert success
@@ -104,27 +114,31 @@ async def test_shell(cmd, expected_stdout, expected_stderr):
     ],
 )
 @pytest.mark.parametrize(
-    "name", [
+    "name",
+    [
         pytest.param("Name 1", id="first"),
         pytest.param("Name 2", id="second"),
-    ]
+    ],
 )
 @pytest.mark.parametrize(
-    "with_prefix", [
+    "with_prefix",
+    [
         pytest.param(True, id="with_prefix"),
         pytest.param(False, id="without_prefix"),
-    ]
+    ],
 )
-async def test_shell_prefix(cmd, expected_stdout, expected_stderr, name, with_prefix):
-    loop = asyncio.get_event_loop()
+async def test_shell_prefix(
+    event_loop, cmd, expected_stdout, expected_stderr, name, with_prefix
+):
+    cmd = get_command(cmd)
     output, errors = [], []
     aiosub = Process(
         *([cmd] if isinstance(cmd, str) else cmd),
-        loop=loop,
+        loop=event_loop,
         stdout=lambda s: output.append(s),
         stderr=lambda s: errors.append(s),
         name=name,
-        with_prefix=with_prefix
+        with_prefix=with_prefix,
     )
     success = await aiosub.shell()
     assert success
@@ -159,16 +173,18 @@ def check_with_prefix(received, expected, with_prefix, name):
         ),
     ],
 )
-async def test_exec(cmd, expected_stdout, expected_stderr):
-    loop = asyncio.get_event_loop()
+async def test_exec(event_loop, cmd, expected_stdout, expected_stderr):
+    cmd = get_command(cmd)
+    expected_stdout = get_command(expected_stdout)
+    expected_stderr = get_command(expected_stderr)
     output, errors = [], []
     aiosub = Process(
         *([cmd] if isinstance(cmd, str) else cmd),
-        loop=loop,
+        loop=event_loop,
         expected_returncode=0,  # Because timeout is expected
         stdout=lambda s: output.append(s),
         stderr=lambda s: errors.append(s),
-        with_prefix=False
+        with_prefix=False,
     )
     success = await aiosub.exec()
     assert success
@@ -194,13 +210,13 @@ async def test_exec(cmd, expected_stdout, expected_stderr):
         ),
     ],
 )
-async def test_realtime_shell(cmd, expected_stdout, expected_stderr):
-    loop = asyncio.get_event_loop()
+async def test_realtime_shell(event_loop, cmd, expected_stdout, expected_stderr):
+    cmd = get_command(cmd)
     output, errors = [], []
     s_res = 0.05
     aiosub = Process(
         cmd,
-        loop=loop,
+        loop=event_loop,
         stdout=lambda s: output.append((datetime.now(), s)),
         stderr=lambda s: errors.append((datetime.now(), s)),
         with_prefix=False,
