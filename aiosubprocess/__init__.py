9aeb8aaa45337c44d2229a7b372bd08f87ee320a
Samuel Giffard <samuel@giffard.co>
2021-05-25 20:42:41 +0900

    Version bump 2021.5.2. Better description.

--- aiosubprocess/__init__.py
+++ aiosubprocess/__init__.py
@@ -1,6 +1,6 @@
-"""Boilerplate for asyncio applications"""
+"""A zero-dependence async subprocess that keeps on getting stdout and stderr."""
 
 from .aiosubprocess import Process
 
-__version__ = "2021.5.1"
+__version__ = "2021.5.2"
 __all__ = ["Process"]
